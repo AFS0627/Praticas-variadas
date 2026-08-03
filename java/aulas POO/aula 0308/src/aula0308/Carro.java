6f71235be1c4650e78b3fe129e644105f729057f
Arthur <arthur.fr.sc@gmail.com>
2026-08-03 20:49:51 -0300

    aula 0308

---
+++ java/aulas POO/aula 0308/src/aula0308/Carro.java
@@ -0,0 +1,26 @@
+package aula0308;
+
+public class Carro {
+
+	int potencia;
+	String placa;
+	String modelo;
+	
+	//métodos
+	
+	void ligar() {
+		System.out.println(this.modelo + " está ligando!");
+	}
+	
+	void desligar() {
+		System.out.println(this.modelo + " está desligando!");
+	}
+	
+	void acelerar(int intensidade) {
+		System.out.println(this.modelo + "Acelerando com intencidade " + intensidade);
+	}
+	
+	void freiar(int intensidade) {
+		System.out.println(this.modelo + "freiando com intencidade " + intensidade);
+	}
+}
---
+++ java/aulas POO/aula 0308/src/aula0308/main.java
@@ -0,0 +1,30 @@
+package aula0308;
+
+public class main {
+
+	public static void main(String[] args) {
+		Carro gol = new Carro();
+		
+		gol.modelo = "GTI";
+		gol.potencia = 120;
+		gol.placa = "XYZ5-A6AB";
+		
+		gol.ligar();
+//		gol.acelerar(20);
+//		gol.freiar(100);
+//		gol.desligar();
+//		
+//		System.out.println("Modelo do carro: " + gol.modelo);
+//		System.out.println("potencia do carro: " + gol.potencia);
+//		System.out.println("placa do carro: " + gol.placa);
+		
+	Carro vectra = new Carro();
+		
+	vectra.modelo = "GT";
+		vectra.potencia = 133;
+		vectra.placa = "FG23-WE45";
+		
+		vectra.ligar();
+	}
+
+}
